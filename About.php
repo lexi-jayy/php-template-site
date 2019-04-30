69c4ce87755f444a74b6265aa176b8cf45368929
Lexi-Jayy Critchley <845869864@bun2-206-18.local>
2019-04-30 15:23:08 +1200

    added p tag

--- About.php
+++ About.php
@@ -7,6 +7,7 @@ require("templates/header.php");
 
 
     <main>
+        <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit.
      Doloribus ad dicta porro perferendis debitis aliquam eveniet odio magni ipsam perspiciatis.
      Exercitationem numquam dolor ducimus quis enim excepturi hic incidunt quos!
@@ -22,6 +23,7 @@ require("templates/header.php");
      Lorem ipsum dolor sit amet consectetur adipisicing elit.
      Doloribus ad dicta porro perferendis debitis aliquam eveniet odio magni ipsam perspiciatis.
      Exercitationem numquam dolor ducimus quis enim excepturi hic incidunt quos!
+     <p>
     </main>
 
 
